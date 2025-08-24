1f102f0e7aac8c74c561f61ed8b8c3ffbd07610a
pam5596 <ryosuke3191@gmail.com>
2025-08-24 16:35:34 +0900

    #6 BaseValueObjectを実装

---
+++ bon-path-backend/src/models/valueObject/_abstruct.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+import { ValidationError } from "@error";
+
+export default abstract class BaseValueObject<T> {
+    protected readonly _value: T;
+
+    constructor(value: T, schema: z.ZodType<T>) {
+        this._value = this.validate(value, schema);
+    }
+
+    validate(value: T, schema: z.ZodType<T>): T {
+        const parse_result = schema.safeParse(value);
+
+        if (!parse_result.success) {
+            const zod_error_issues = parse_result.error.issues
+            throw new ValidationError(
+                zod_error_issues.map((issue) => issue.message).join("\n"),
+                zod_error_issues.map((issue) => issue.message).join("\n"),
+                this.constructor.name
+            );
+        }
+        return parse_result.data;
+    }
+
+
+}
