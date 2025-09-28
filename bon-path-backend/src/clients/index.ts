3a20f88571bd554c20b1fc42bbea495b1cb3b01d
pam5596 <ryosuke3191@gmail.com>
2025-09-28 11:19:21 +0900

    #18 Argon2Clientとテストを実装

--- bon-path-backend/src/clients/index.ts
+++ bon-path-backend/src/clients/index.ts
@@ -3,3 +3,4 @@ export { PrismaClient } from "./prisma";
 export { LangChainOpenAiClient } from "./openAi";
 export { GoogleSearchAPIClient } from "./googleSearch";
 export { GoogleMapPlacesAPIClient } from "./googleMapPlaces";
+export { Argon2Client } from "./argon2";
