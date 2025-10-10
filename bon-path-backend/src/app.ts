2c90f9470a4fd07a238e2ab00046e250a938254f
pam5596 <ryosuke3191@gmail.com>
2025-10-10 20:12:02 +0900

    #32 SwaggerUI用のルートを追加

--- bon-path-backend/src/app.ts
+++ bon-path-backend/src/app.ts
@@ -1,11 +1,21 @@
-import { Hono } from 'hono'
+import { OpenAPIHono } from '@hono/zod-openapi'
+import { swaggerUI } from "@hono/swagger-ui";
 import { serve } from '@hono/node-server'
 
-const app = new Hono()
+const app = new OpenAPIHono()
 
 app.get('/', (c) => {
     return c.text('Hello Hono!')
 })
+
+app.doc('/doc', {
+    openapi: '3.0.0',
+    info: {
+        version: '1.0.0',
+        title: 'BonPathAPI',
+    },
+})
+app.get("/docs", swaggerUI({ url: "/doc" }))
 
 serve({
     fetch: app.fetch,
