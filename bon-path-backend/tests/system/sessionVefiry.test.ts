bfd7a53d6b01a9163eea3da1b0741c493b3a1b56
pam5596 <ryosuke3191@gmail.com>
2025-10-18 14:16:25 +0900

    #33 /session/verifyのシステムテストを実装

---
+++ bon-path-backend/tests/system/sessionVefiry.test.ts
@@ -0,0 +1,46 @@
+import { describe, expect, test } from "vitest";
+import { createVerifySession } from "./_createVerifySession";
+import { request } from "./_request";
+import { withTestFixtures } from "./_withTestFixtures";
+import { prisma } from "@lib/clients";
+
+describe('sessionVerifyエンドポイントのシステムテスト', () => {
+    withTestFixtures(prisma)
+
+    test('[POST]sessionVerify', async () => {
+        const res = await request(`/session/verify`, {
+            method: 'POST',
+            body: JSON.stringify({
+                name: "name",
+                email: "email@example.com",
+                password: "password"
+            }),
+            redirect: "manual"
+        });
+                
+        expect(res.status).toBe(302)
+        expect(res.headers.getSetCookie()[0].includes('verifySessionId'))
+        expect(res.headers.get('location')).toBe(process.env.FRONTEND_DOMAIN + '/signin/email-verify')
+    })
+
+    test('[GET]sessionVerify', async () => {
+        const verifySessionId = await createVerifySession({
+            name: "name",
+            email: "email@example.com",
+            password: "password"
+        })
+
+        const res = await request('/session/verify', {
+            method: 'GET',
+            headers: new Headers({
+                'Cookie': verifySessionId
+            })
+        })
+
+        const body = await res.json()
+
+        expect(res.status).toBe(200)
+        expect(body.userName).toBe('name')
+        expect(body.userEmail).toBe('email@example.com')
+    })
+})
