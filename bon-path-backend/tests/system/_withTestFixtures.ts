6b6cab64b4ac27db01536291bbcc82b7a3bbabf4
pam5596 <ryosuke3191@gmail.com>
2025-10-20 00:13:25 +0900

    #33 fixturesを挿入するcallback関数を用意

--- bon-path-backend/tests/system/_withTestFixtures.ts
+++ bon-path-backend/tests/system/_withTestFixtures.ts
@@ -9,7 +9,9 @@ import category_fixtures from "@share/fixtures/categories.json";
 import product_fixtures from "@share/fixtures/products.json";
 import purchase_fixtures from "@share/fixtures/purchases.json";
 
-export function withTestFixtures(client: PrismaClient) {
+export function withTestFixtures(
+    client: PrismaClient
+) {
     beforeAll(
         async () => {
             await client.$executeRawUnsafe(`
@@ -25,13 +27,71 @@ export function withTestFixtures(client: PrismaClient) {
                     "ProductVector"
                 RESTART IDENTITY CASCADE;
             `)
+
             await client.user.createMany({ data: user_fixtures })
+            await client.$executeRawUnsafe(`
+                SELECT setval(
+                    pg_get_serial_sequence('"User"', 'id'),
+                    COALESCE((SELECT MAX(id) FROM "User"), 0) + 1,
+                    false
+                );
+            `);
+
+
             await client.receipt.createMany({ data: receipt_fixtures })
+            await client.$executeRawUnsafe(`
+                SELECT setval(
+                    pg_get_serial_sequence('"Receipt"', 'id'),
+                    COALESCE((SELECT MAX(id) FROM "Receipt"), 0) + 1,
+                    false
+                );
+            `);
+            
             await client.receiptImage.createMany({ data: receipt_image_fixtures })
+            await client.$executeRawUnsafe(`
+                SELECT setval(
+                    pg_get_serial_sequence('"ReceiptImage"', 'id'),
+                    COALESCE((SELECT MAX(id) FROM "ReceiptImage"), 0) + 1,
+                    false
+                );
+            `);
+
+
             await client.store.createMany({ data: store_fixtures })
+            await client.$executeRawUnsafe(`
+                SELECT setval(
+                    pg_get_serial_sequence('"Store"', 'id'),
+                    COALESCE((SELECT MAX(id) FROM "Store"), 0) + 1,
+                    false
+                );
+            `);
+            
             await client.category.createMany({ data: category_fixtures })
+            await client.$executeRawUnsafe(`
+                SELECT setval(
+                    pg_get_serial_sequence('"Category"', 'id'),
+                    COALESCE((SELECT MAX(id) FROM "Category"), 0) + 1,
+                    false
+                );
+            `);
+
             await client.product.createMany({ data: product_fixtures })
+            await client.$executeRawUnsafe(`
+                SELECT setval(
+                    pg_get_serial_sequence('"Product"', 'id'),
+                    COALESCE((SELECT MAX(id) FROM "Product"), 0) + 1,
+                    false
+                );
+            `);
+            
             await client.purchase.createMany({ data: purchase_fixtures })
+            await client.$executeRawUnsafe(`
+                SELECT setval(
+                    pg_get_serial_sequence('"Purchase"', 'id'),
+                    COALESCE((SELECT MAX(id) FROM "Purchase"), 0) + 1,
+                    false
+                );
+            `);
         }
     )
 
