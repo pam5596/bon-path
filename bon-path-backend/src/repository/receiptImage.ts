decf983952a44afd589521153d14c6a61ef24758
pam5596 <ryosuke3191@gmail.com>
2025-10-20 19:06:13 +0900

    #33 ReceiptImageRepositoryからinsertManyをinsertに代替

--- bon-path-backend/src/repository/receiptImage.ts
+++ bon-path-backend/src/repository/receiptImage.ts
@@ -1,16 +1,18 @@
 import BaseRepository from "./_abstruct";
 import queryHandler from "./_queryHandler";
-import { Id } from "@models/valueObject";
+import { CreatedAt, Id } from "@models/valueObject";
 import { ReceiptImageEntity } from "@models/entity";
 
 export default class ReceiptImageRepository extends BaseRepository {
     @queryHandler
-    async insertMany(receiptImages: ReceiptImageEntity[]) {
-        const create_result = await this.client.receiptImage.createManyAndReturn({
-            data: receiptImages.map((receiptImage) => receiptImage.toPrimitives)
+    async insert(receiptImage: ReceiptImageEntity) {
+        const create_result = await this.client.receiptImage.create({
+            data: receiptImage.toPrimitives
         });
-
-        return create_result.map((receiptImage) => ReceiptImageEntity.fromPrimitives(receiptImage));
+        
+        receiptImage.newId = new Id(create_result.id)
+        receiptImage.created = new CreatedAt(create_result.createdAt)
+        return receiptImage
     }
 
     @queryHandler
