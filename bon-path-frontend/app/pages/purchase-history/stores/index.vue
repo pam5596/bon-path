c3c9301dd97dc6d4cde16d041fc0866256b0d33b
pam5596 <ryosuke3191@gmail.com>
2025-10-24 21:19:11 +0900

    #46 /purchase-history/storesのUXを実装

--- bon-path-frontend/app/pages/purchase-history/stores/index.vue
+++ bon-path-frontend/app/pages/purchase-history/stores/index.vue
@@ -1,6 +1,20 @@
 <template>
     <div>
         <h1>PurchaseHistoryStores</h1>
+        <ul>
+            <li>
+                <p>店名</p>
+                <button>ここでの履歴を見る</button>
+            </li>
+            <li>
+                <p>店名</p>
+                <button>ここでの履歴を見る</button>
+            </li>
+            <li>
+                <p>店名</p>
+                <button>ここでの履歴を見る</button>
+            </li>
+        </ul>
     </div>
 </template>
 
