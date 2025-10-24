8b934e60b475985545ffae224fa33fde47a3b88a
pam5596 <ryosuke3191@gmail.com>
2025-10-24 21:20:35 +0900

    #46 /purchase-history/stores/[storeId]/receiptsのUXを実装

--- bon-path-frontend/app/pages/purchase-history/stores/[storeId]/receipts/index.vue
+++ bon-path-frontend/app/pages/purchase-history/stores/[storeId]/receipts/index.vue
@@ -2,6 +2,24 @@
     <div>
         <h1>PurchaseHistoryReceipts</h1>
         <p>{{ $route.params }}</p>
+        <p>店名</p>
+        <ul>
+            <li>
+                <p>日時</p>
+                <button>商品を見る</button>
+                <button>削除</button>
+            </li>
+            <li>
+                <p>日時</p>
+                <button>商品を見る</button>
+                <button>削除</button>
+            </li>
+            <li>
+                <p>日時</p>
+                <button>商品を見る</button>
+                <button>削除</button>
+            </li>
+        </ul>
     </div>
 </template>
 
