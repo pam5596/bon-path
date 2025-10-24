5bcf66999d1db60ebae4f7648315e6cd2ca37aa3
pam5596 <ryosuke3191@gmail.com>
2025-10-24 20:17:13 +0900

    #46 /receipt-register/photoのUXを実装

--- bon-path-frontend/app/pages/dashboard.vue
+++ bon-path-frontend/app/pages/dashboard.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
         <h1>Dashboard</h1>
+        <p>アカウント名</p>
+        <ul>
+            <li>未確認のレシート</li>
+            <li>未確認のレシート</li>
+            <li>未確認のレシート</li>
+        </ul>
     </div>
 </template>
 
--- bon-path-frontend/app/pages/receipt-register/photo.vue
+++ bon-path-frontend/app/pages/receipt-register/photo.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
         <h1>ReceiptRegisterPhoto</h1>
+        <button>撮影</button>
+
+        <ul>
+            <li>レシートの画像</li>
+            <li>レシートの画像</li>
+            <li>レシートの画像</li>
+        </ul>
+        <button>登録する</button>
     </div>
 </template>
 
