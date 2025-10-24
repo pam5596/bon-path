b5d04bfbcf8839e57e38853e4757ba1fb0cce051
pam5596 <ryosuke3191@gmail.com>
2025-10-24 19:10:16 +0900

    #46 /signupのUXを実装

--- bon-path-frontend/app/pages/signup.vue
+++ bon-path-frontend/app/pages/signup.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
         <h1>Signup</h1>
+        <form>
+            <input placeholder="email">
+            <input placeholder="password">
+            <button type="submit">送信</button>
+        </form>
+        <button>アカウント登録はこちら</button>
     </div>
 </template>
 
