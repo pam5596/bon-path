a0116020607d619d37f0916abdfdc5179b870c28
pam5596 <ryosuke3191@gmail.com>
2025-10-26 18:26:36 +0900

    #51 UserRepositoryを実装

---
+++ bon-path-frontend/app/repositories/user.ts
@@ -0,0 +1,55 @@
+import type { UserPayloads } from '@share/payloads'
+import type { UserModel } from '~/models'
+
+export class UserRepository {
+    async post() {
+        return await useAPIFetch<UserPayloads.POST.Response['body']>(
+            '/users', {
+                method: 'post'
+            }
+        )
+    }
+
+    async get() {
+        return await useAPIFetch<UserPayloads.GET.Response['body']>(
+            '/users', {
+                method: 'get'
+            }
+        )
+    }
+
+    async getReceipts(query?: UserPayloads.Receipts.GET.Request['query']) {
+        return await useAPIFetch<UserPayloads.Receipts.GET.Response['body']>(
+            '/users/receipts', {
+                method: 'get',
+                query
+            }
+        )
+    }
+
+    async getPurchases() {
+        return await useAPIFetch<UserPayloads.Purchases.GET.Response['body']>(
+            '/users/purchases', {
+                method: 'get',
+            }
+        )
+    }
+
+    async patch(payload: UserModel) {
+        const { name, email } = payload.getValues
+        return await useAPIFetch<undefined>(
+            '/users', {
+                method: 'patch',
+                body: { name, email }
+            }
+        )
+    }
+
+    async delete() {
+        return await useAPIFetch<undefined>(
+            '/users', {
+                method: 'delete',
+            }
+        )
+    }
+}
