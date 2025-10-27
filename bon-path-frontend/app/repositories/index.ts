b1310e5cefef642c9265726f817ffa8995f51b1a
pam5596 <ryosuke3191@gmail.com>
2025-10-27 20:27:27 +0900

    #51 ReceiptImageRepositoryを実装

--- bon-path-frontend/app/repositories/index.ts
+++ bon-path-frontend/app/repositories/index.ts
@@ -1,3 +1,5 @@
 export { UserRepository } from "./user";
 export { SessionLoginRepository } from "./sessionLogin";
 export { SessionVerifyRepository } from "./sessionVerify";
+export { ReceiptRepository } from "./receipt";
+export { ReceiptImageRepository } from "./receiptImage";
