3b7b64b022ea0e2b680aeaa4414672a9d7a2a01d
pam5596 <ryosuke3191@gmail.com>
2025-10-28 23:39:53 +0900

    #54 アラートのダイアログをlayoutに実装

--- bon-path-frontend/app/app.vue
+++ bon-path-frontend/app/app.vue
@@ -2,8 +2,10 @@
   <div>
     <v-app>
       <NuxtLayout>
-        <NuxtLayout name="loading-overlay">
-          <NuxtPage />
+        <NuxtLayout name="alert-dialog">
+          <NuxtLayout name="loading-overlay">
+            <NuxtPage />
+          </NuxtLayout>
         </NuxtLayout>
       </NuxtLayout>
     </v-app>
---
+++ bon-path-frontend/app/layouts/alertDialog.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <slot />
+        <v-dialog>
+            <v-alert></v-alert>
+        </v-dialog>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
