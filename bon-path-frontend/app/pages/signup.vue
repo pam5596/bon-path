768b24640ab0f2607d4d79c7973f60aca9130394
pam5596 <ryosuke3191@gmail.com>
2025-10-28 14:27:59 +0900

    #54 /signupのコンポーネント群をVuetifyに書き換え

--- bon-path-frontend/app/pages/signup.vue
+++ bon-path-frontend/app/pages/signup.vue
@@ -1,12 +1,11 @@
 <template>
     <div>
-        <h1>Signup</h1>
-        <form>
-            <input placeholder="email">
-            <input placeholder="password">
-            <button type="submit">ログイン</button>
-        </form>
-        <button>アカウント登録はこちら</button>
+        <v-form>
+            <v-text-field label="メールアドレス" />
+            <v-text-field label="パスワード" />
+            <v-btn>ログイン</v-btn>
+        </v-form>
+        <v-btn>アカウント登録はこちら</v-btn>
     </div>
 </template>
 
