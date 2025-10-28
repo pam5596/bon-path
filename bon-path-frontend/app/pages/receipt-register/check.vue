a582736a041b4f5d6bc60697cc56f1178b16411e
pam5596 <ryosuke3191@gmail.com>
2025-10-28 21:13:47 +0900

    #54 /receipt-register/checkのコンポーネント群をVuetifyに書き換え

--- bon-path-frontend/app/pages/receipt-register/check.vue
+++ bon-path-frontend/app/pages/receipt-register/check.vue
@@ -1,26 +1,33 @@
 <template>
     <div>
-        <h1>ReceiptRegisterCheck</h1>
-        <p>店名</p>
-        <ul>
-            <li>店舗の画像</li>
-            <li>店舗の画像</li>
-            <li>店舗の画像</li>
-        </ul>
-        <p>日付</p>
-        <ul>
-            <li>
-                <p>商品名</p>
-                <p>価格</p>
-                <p>数量</p>
-                <ul>
-                    <li>商品の画像</li>
-                    <li>商品の画像</li>
-                    <li>商品の画像</li>
-                </ul>
-            </li>
-        </ul>
-        <button>登録する</button>
+        <v-parallax></v-parallax>
+        <v-card>
+            <v-card-title>
+                2025/10/28 Fri. 10:00
+            </v-card-title>
+            <v-card-text>
+                <v-form>
+                    <div>
+                        <h2>店舗</h2>
+                        <v-avatar></v-avatar>
+                        <v-autocomplete></v-autocomplete>
+                    </div>
+                    <v-divider></v-divider>
+                    <div>
+                        <h2>商品一覧</h2>
+                        <v-list>
+                            <v-list-item>
+                                <v-avatar></v-avatar>
+                                <v-autocomplete></v-autocomplete>
+                                <p>100円</p>
+                                <p>×1</p>
+                            </v-list-item>
+                        </v-list>
+                    </div>
+                    <v-btn>登録する</v-btn>
+                </v-form>
+            </v-card-text>
+        </v-card>
     </div>
 </template>
 
