cc013715282a053ab990207e05c95ace5ca1ba3f
pam5596 <ryosuke3191@gmail.com>
2025-10-28 14:29:11 +0900

    #54 /signinのコンポーネント群をVuetifyに書き換え

--- bon-path-frontend/app/pages/signin.vue
+++ bon-path-frontend/app/pages/signin.vue
@@ -1,13 +1,12 @@
 <template>
     <div>
-        <h1>Signin</h1>
-        <form>
-            <input placeholder="name">
-            <input placeholder="email">
-            <input placeholder="password">
-            <button type="submit">アカウント登録</button>
-        </form>
-        <button>ログインはこちら</button>
+        <v-form>
+            <v-text-field label="名前" />
+            <v-text-field label="メールアドレス" />
+            <v-text-field label="パスワード" />
+            <v-btn>アカウント登録</v-btn>
+        </v-form>
+        <v-btn>ログインはこちら</v-btn>
     </div>
 </template>
 
