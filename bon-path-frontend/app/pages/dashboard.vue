b3928806f0a740231d3781d4710690f3be8f6d41
pam5596 <ryosuke3191@gmail.com>
2025-10-28 15:48:34 +0900

    #54 /dashboardのコンポーネント群をVuetifyに書き換え

--- bon-path-frontend/app/pages/dashboard.vue
+++ bon-path-frontend/app/pages/dashboard.vue
@@ -1,12 +1,28 @@
 <template>
     <div>
-        <h1>Dashboard</h1>
-        <p>アカウント名</p>
-        <ul>
-            <li>未確認のレシート</li>
-            <li>未確認のレシート</li>
-            <li>未確認のレシート</li>
-        </ul>
+        <h1>ダッシュボード</h1>
+        <h2>アカウント名</h2>
+        <h2>登録が終わってないレシートがあります！</h2>
+        <div>
+            <v-card>
+                <v-avatar>
+                    <v-img></v-img>
+                </v-avatar>
+                <v-card-item>
+                    <v-card-title>
+                        2025/10/28 Fri. 14:00
+                    </v-card-title>
+                    <v-card-actions>
+                        <v-btn>
+                            今すぐ登録する
+                        </v-btn>
+                        <v-btn>
+                            削除する
+                        </v-btn>
+                    </v-card-actions>
+                </v-card-item>
+            </v-card>
+        </div>
     </div>
 </template>
 
