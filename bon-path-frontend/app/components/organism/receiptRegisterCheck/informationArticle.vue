2f7d6bc346eeb976466e554ee28c01a918417706
pam5596 <ryosuke3191@gmail.com>
2025-10-30 10:12:48 +0900

    #60 /receipt-register/checkをorganismにセクション分け

---
+++ bon-path-frontend/app/components/organism/receiptRegisterCheck/informationArticle.vue
@@ -0,0 +1,39 @@
+<template>
+    <article>
+        <v-card>
+            <v-card-title>
+                2025/10/28 Fri. 10:00
+            </v-card-title>
+            <v-card-text>
+                <v-form>
+                    <div>
+                        <h2>店舗</h2>
+                        <v-avatar></v-avatar>
+                        <v-autocomplete></v-autocomplete>
+                    </div>
+                    <v-divider></v-divider>
+                    <div>
+                        <h2>商品一覧</h2>
+                        <div>
+                            <div>
+                                <v-avatar></v-avatar>
+                                <v-autocomplete></v-autocomplete>
+                                <p>100円</p>
+                                <p>×1</p>
+                            </div>
+                        </div>
+                    </div>
+                    <v-btn>登録する</v-btn>
+                </v-form>
+            </v-card-text>
+        </v-card>
+    </article>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
