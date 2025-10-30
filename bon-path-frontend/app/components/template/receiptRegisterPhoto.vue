f51781f007182204b6fe40e9c305fb465e80c2c7
pam5596 <ryosuke3191@gmail.com>
2025-10-30 10:19:54 +0900

    #60 /receipt-register/showをorganismにセクション分け

--- bon-path-frontend/app/components/template/receiptRegisterPhoto.vue
+++ bon-path-frontend/app/components/template/receiptRegisterPhoto.vue
@@ -1,32 +1,9 @@
 <template>
     <main>
-        <div>
-            <!-- [molecule]ここにcamera canvas -->
-            <v-btn>撮影</v-btn>
-            <v-btn>次へ</v-btn>
-        </div>
-        <v-dialog>
-            <v-card>
-                <v-card-title>
-                    撮影したレシート
-                </v-card-title>
-                <v-card-text>
-                    <v-avatar>
-                        <v-img></v-img>
-                    </v-avatar>
-                    <v-avatar>
-                        <v-img></v-img>
-                    </v-avatar>
-                    <v-avatar>
-                        <v-img></v-img>
-                    </v-avatar>
-                </v-card-text>
-                <v-card-actions>
-                    <v-btn>登録する</v-btn>
-                    <v-btn>キャンセル</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
+        <article>
+            <OrganismReceiptRegisterPhotoCameraSection />
+            <OrganismReceiptRegisterPhotoShowSection />
+        </article>
         <OrganismHomeNavigation />
     </main>
 </template>
