b2089861fbd6339018afd98baa6dcddafe43e346
pam5596 <ryosuke3191@gmail.com>
2025-10-30 09:59:58 +0900

    #60 /email-verifyをorganismにセクション分け

--- bon-path-frontend/app/components/template/emailVerify.vue
+++ bon-path-frontend/app/components/template/emailVerify.vue
@@ -1,10 +1,6 @@
 <template>
     <main>
-        <article>
-            <h1>アカウント登録に成功しました</h1>
-            <p>※ベータ版ではメールアドレスの確認はありません</p>
-            <v-btn>ログインへ</v-btn>
-        </article>
+        <OrganismEmailVerifySuccessMessageArticle />
     </main>
 </template>
 
