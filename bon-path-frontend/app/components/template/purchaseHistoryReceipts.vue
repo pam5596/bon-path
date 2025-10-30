4e0fb14061f3c2d3d0a4decfdf0c42bcdee9edb7
pam5596 <ryosuke3191@gmail.com>
2025-10-30 10:06:12 +0900

    #60 /purchase-history/~/receiptsをorganismにセクション分け

--- bon-path-frontend/app/components/template/purchaseHistoryReceipts.vue
+++ bon-path-frontend/app/components/template/purchaseHistoryReceipts.vue
@@ -2,29 +2,7 @@
     <main>
         <!-- <p>{{ $route.params }}</p> -->
         <v-parallax></v-parallax>
-        <article>
-            <h1>店名</h1>
-            <div>
-                <v-card>
-                    <v-avatar>
-                        <v-img></v-img>
-                    </v-avatar>
-                    <v-card-item>
-                        <v-card-title>
-                            2025/10/28 Fri. 10:00
-                        </v-card-title>
-                        <v-card-actions>
-                            <v-btn>
-                                商品を見る
-                            </v-btn>
-                            <v-btn>
-                                削除
-                            </v-btn>
-                        </v-card-actions>
-                    </v-card-item>
-                </v-card>
-            </div>
-        </article>
+        <OrganismPurchaseHistoryReceiptsStoreReceiptsArticle />
         <OrganismHomeNavigation />
     </main>
 </template>
