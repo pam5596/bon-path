5510d2b79bb0420bbe3e0e93afc97f05ea2f08cd
pam5596 <ryosuke3191@gmail.com>
2025-10-30 10:21:23 +0900

    #60 /signinをorganismにセクション分け

--- bon-path-frontend/app/components/template/signin.vue
+++ bon-path-frontend/app/components/template/signin.vue
@@ -1,11 +1,6 @@
 <template>
     <main>
-        <v-form>
-            <v-text-field label="名前" />
-            <v-text-field label="メールアドレス" />
-            <v-text-field label="パスワード" />
-            <v-btn>アカウント登録</v-btn>
-        </v-form>
+        <OrganismSigninForm />
         <v-btn>ログインはこちら</v-btn>
     </main>
 </template>
