f87b93e590c3a5f949cb156f108564ecdb41bdb0
pam5596 <ryosuke3191@gmail.com>
2025-10-31 18:50:48 +0900

    #67 userPurchaseHistoryReceiptsViewModelを定義

--- bon-path-frontend/app/composables/viewModels/purchaseHistoryReceipts.ts
+++ bon-path-frontend/app/composables/viewModels/purchaseHistoryReceipts.ts
@@ -1,18 +1,15 @@
 export default function() {
     // [ref]: 
     // * store: StoreModel - 店舗情報
-    // * receipt: ReceiptModel - レシート情報
+    // * receipts: ReceiptModel[] - レシート情報
     // * receiptImages: ReceiptImage[] - レシート画像
-    // * purchases: Purchases[] - 購入履歴
-    // * products: Products[] - 商品情報
     
     // [mount]:
     // * [GET] /stores/:storeId - 店舗情報を取得する
     // * [GET] /receipts/:receiptId - レシート情報を取得する
     // * [GET] /receipts/:receiptId/images - レシート画像を取得する
-    // * [GET] /receipts/:receiptId/purchases - レシートの購入履歴を取得する
-    // * [GET] /products/:productId - 商品情報を取得する
     
     // [events]:
+    // * toReceipt: ボタン押下で/purchase-history/stores/[storeId]/receiptへ遷移する
     // * onDeleteReceipt: ボタン押下でレシートを削除する
 }
