3b2410729d4455e70aa425e0a32a2b5d93fd254c
pam5596 <ryosuke3191@gmail.com>
2025-10-31 13:28:45 +0900

    #64 /receipt-register/checkのproductListItemをproductSelectorに分離

--- bon-path-frontend/app/components/molecule/receiptRegisterCheck/productListItem.vue
+++ bon-path-frontend/app/components/molecule/receiptRegisterCheck/productListItem.vue
@@ -1,7 +1,7 @@
 <template>
     <v-list-item>
         <v-list-img></v-list-img>
-        <v-autocomplete></v-autocomplete>
+        <MoleculeReceiptRegisterCheckProductSelector />
         <v-text-field>100円</v-text-field>
         <v-text-field>×1</v-text-field>
     </v-list-item>
---
+++ bon-path-frontend/app/components/molecule/receiptRegisterCheck/productSelector.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-autocomplete></v-autocomplete>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
