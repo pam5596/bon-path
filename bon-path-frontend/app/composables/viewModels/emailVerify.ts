540a50fa5a1dd6b152a1cb57c8d386f1af2dd22b
pam5596 <ryosuke3191@gmail.com>
2025-10-31 18:24:23 +0900

    #67 userEmailVerifyViewModelを定義

---
+++ bon-path-frontend/app/composables/viewModels/emailVerify.ts
@@ -0,0 +1,11 @@
+export default function() {
+    // [ref]: 
+    // * userHashedId: string - ユーザーのハッシュID
+    
+    // [mount]:
+    // * [POST]/users
+
+    
+    // [events]:
+    // * toSignUp: ボタン押下で/signupへ遷移する
+}
