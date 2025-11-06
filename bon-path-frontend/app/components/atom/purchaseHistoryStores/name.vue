32f3e424bb8e2975aac076593e45c4b1088c1462
pam5596 <ryosuke3191@gmail.com>
2025-11-07 01:18:11 +0900

    #65 /purchase-history/storesで反映されるi18nを実装

--- bon-path-frontend/app/components/atom/purchaseHistoryStores/name.vue
+++ bon-path-frontend/app/components/atom/purchaseHistoryStores/name.vue
@@ -1,6 +1,6 @@
 <template>
     <v-card-title>
-        name
+        <slot />
     </v-card-title>
 </template>
 
---
+++ bon-path-frontend/app/components/organism/purchaseHistoryStores/listArticle.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-for="(store, index) in stores" :key="index">
+        <MoleculePurchaseHistoryStoresCard 
+            :store="store"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+const { storesFixture } = useFixtures()
+const stores = computed(()=>storesFixture.map(
+    store => new StoreModel(store))
+)
+
+</script>
+
+<style scoped>
+
+</style>
