618b1331f69bf61830cfc6502373f4dcd760c467
pam5596 <ryosuke3191@gmail.com>
2025-11-07 02:43:23 +0900

    #65 /signin, /signupで反映されるi18nを実装

--- bon-path-frontend/app/components/organism/signup/form.vue
+++ bon-path-frontend/app/components/organism/signup/form.vue
@@ -1,8 +1,10 @@
 <template>
     <v-form>
-        <v-text-field label="メールアドレス" />
-        <v-text-field label="パスワード" />
-        <v-btn>ログイン</v-btn>
+        <v-text-field :label="$t('signUp.form.email')" />
+        <v-text-field :label="$t('signUp.form.password')" />
+        <v-btn>
+            {{ $t("signUp.form.onSubmitBtn") }}
+        </v-btn>
     </v-form>
 </template>
 
