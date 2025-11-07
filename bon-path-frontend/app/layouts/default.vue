45048ca9092a079fbf2d1347b352eda9219e9753
pam5596 <ryosuke3191@gmail.com>
2025-11-07 14:02:08 +0900

    #78 defaultLayoutのスタイリングを実装する

--- bon-path-frontend/app/layouts/default.vue
+++ bon-path-frontend/app/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="layout-default">
         <slot />
     </div>
 </template>
@@ -9,5 +9,10 @@
 </script>
 
 <style scoped>
+.layout-default {
+    padding: 1rem;
+    background: linear-gradient(#b1aaff, #8ec5fc);
+    height: 100vh;
+}
 
 </style>
