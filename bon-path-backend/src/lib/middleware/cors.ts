56452318e1db5fa3c4a1081c7d834af6be160cf0
pam5596 <ryosuke3191@gmail.com>
2025-11-13 18:53:58 +0900

    #1 develop-backend: corsのoriginを制御

--- bon-path-backend/src/lib/middleware/cors.ts
+++ bon-path-backend/src/lib/middleware/cors.ts
@@ -1,3 +1,9 @@
 import { cors } from "hono/cors";
 
-export const corsHandler = cors()
+export const corsHandler = cors({
+    origin: [
+        'http://localhost:3000',
+        process.env.FRONTEND_DOMAIN || ''
+    ],
+    credentials: true
+})
