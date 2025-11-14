9f9fb389ee349da5fc8b130aaee65e1699dd37b4
pam5596 <ryosuke3191@gmail.com>
2025-11-15 01:42:24 +0900

    #85 deleteReceiptImageを追加実装

--- bon-path-frontend/app/composables/usecases/onDeleteReceipt.ts
+++ bon-path-frontend/app/composables/usecases/onDeleteReceipt.ts
@@ -1,10 +1,12 @@
 export default function (refresher: () => void) {
     const { deleteReceipt } = useReceipts()
+    const { deleteReceiptImage } = useReceiptImages()
 
     return useAsyncOnEvent(
         async (id: number) => {
             await deleteReceipt({ params: { id }})
             refresher()
+            await deleteReceiptImage({ params: { id }})
         }
     )
 }
